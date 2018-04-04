c0b933b760e29ed4d02f472f97c1292011a68829
Sidjulik <36816365+Sidjulik@users.noreply.github.com>
2018-04-04 23:09:27 +0200

    предитоги

---
+++ js/script.js
@@ -0,0 +1,24 @@
+	var searchButton = document.querySelector(".search-button");
+	var popup = document.querySelector(".modal-search");
+	
+	var form = popup.querySelector("form");
+	var checkin = popup.querySelector("[name=checkin]");
+	var checkout = popup.querySelector("[name=search-checkout]");
+
+	searchButton.addEventListener("click", function (evt) {
+		evt.preventDefault();
+		popup.classList.toggle("modal-show");
+		popup.classList.remove("modal-error");
+		checkin.focus();				
+	});
+
+	form.addEventListener("submit", function (evt) {
+		if (!checkin.value || !checkout.value || adult.value) {
+		evt.preventDefault();
+		popup.classList.add("modal-error")
+	}
+	});
+
+
+
+
