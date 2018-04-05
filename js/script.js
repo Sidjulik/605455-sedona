0d7ba75db30e9015c3a7606252f2bb6f28612c30
Sidjulik <36816365+Sidjulik@users.noreply.github.com>
2018-04-05 18:05:46 +0200

    последний

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-	var searchButton = document.querySelector(".search-button");
-	var popup = document.querySelector(".modal-search");
+var searchButton = document.querySelector(".search-button");
+var popup = document.querySelector(".modal-search");
 
-	var form = popup.querySelector("form");
-	var checkin = popup.querySelector("[name=checkin]");
-	var checkout = popup.querySelector("[name=search-checkout]");
+var form = popup.querySelector("form");
+var checkin = popup.querySelector("[name=checkin]");
+var checkout = popup.querySelector("[name=search-checkout]");
 
-	document.documentElement.classList.replace("no-js", "js");
+document.documentElement.classList.replace("no-js", "js");
 
-	searchButton.addEventListener("click", function(evt) {
-	  evt.preventDefault();
-	  popup.classList.toggle("modal-show");
-	  popup.classList.remove("modal-error");
-	  checkin.focus();
-	});
+searchButton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.toggle("modal-show");
+  popup.classList.remove("modal-error");
+  checkin.focus();
+});
 
-	form.addEventListener("submit", function(evt) {
-	  if (!checkin.value || !checkout.value || adult.value) {
-	    evt.preventDefault();
-	    popup.classList.add("modal-error");
-	  }
-	});
+form.addEventListener("submit", function(evt) {
+  if (!checkin.value || !checkout.value || adult.value) {
+    evt.preventDefault();
+    popup.classList.add("modal-error");
+  }
+});
